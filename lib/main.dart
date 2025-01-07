6954dc7b5d16942e440743e28a0aabe8848ca3f9
kwontaebin <144872939+Kwontaebin@users.noreply.github.com>
2025-01-07 15:53:22 +0900

    수정 사항 주석 작성

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chatting_app/login/view/login.dart';
 import 'package:flutter_chatting_app/splash/view/splash.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(
