ab48a323a3469a29078d28fe21b27cead4d9bd9e
kwontaebin <144872939+Kwontaebin@users.noreply.github.com>
2025-01-14 15:40:49 +0900

    data.dart 파일 깃에서 삭제

--- lib/common/const/data.dart
+++
@@ -1,9 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-const IP = "http://localhost:3000";
-
-Future<SharedPreferences> prefs() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-
-  return prefs;
-}
